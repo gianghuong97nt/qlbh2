43927c9a5a2e0ac2d85a33bbb3ed659f0cbc5431
gianghuong97nt <gianghuong97nt@gmail.com>
2019-07-23 17:06:50 +0700

    init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role')->default(0);
+            $table->date('birthday')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('phone_number')->nullable();
+            $table->integer('gender')->nullable();
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
